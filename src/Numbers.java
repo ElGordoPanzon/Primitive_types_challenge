c707f16c13b21668aec08c9123b5190b7249614b
jose jimenez <jimenjos000@warren.k12.in.us>
2024-08-05 13:38:26 -0400

    WHAAA

---
+++ src/Numbers.java
@@ -0,0 +1,8 @@
+public class Numbers {
+    public static void main(String[] args) {
+        byte bytevalue = 30;
+        short shortvalue = 50;
+        int intvalue = 60;
+        long longvalue = 80000 - 5 * (bytevalue + shortvalue + intvalue);
+    }
+}
